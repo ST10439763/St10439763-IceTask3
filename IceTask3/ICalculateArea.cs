06ca79bd02a372b6690f5adf7be1f1e88543c363
user <user@LAPTOP-KDTBSEQV>
2025-03-13 14:30:45 +0200

    Add project files.

---
+++ IceTask3/ICalculateArea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTask3
+{
+    interface ICalculateArea
+    {
+        public void calculateArea();
+    }
+}
---
+++ IceTask3/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTask3
+{
+    abstract class Shape
+    {
+        public string Name { get; set; }
+
+        public Shape(string name) 
+        {
+            this.Name = name;
+        }
+
+        public virtual void Display()
+        { }
+    }
+}
